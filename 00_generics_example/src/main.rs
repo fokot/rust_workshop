70ce18dfb6a228eedc5d8351f1c7f8d2575bdb01
fokot <frantisek.kocun@gmail.com>
2019-12-12 10:59:08 +0100

    first

--- 00_generics_example/src/main.rs
+++ 00_generics_example/src/main.rs
@@ -3,10 +3,12 @@ fn noop<T>(val: T) -> T {
 }
 
 fn show_size<T>(_value: T) {
+    // std::mem::size_of - function which returns size in Bytes
     println!("The size is {}", std::mem::size_of::<T>());
 }
 
 fn main() {
+    // by default 0 = 0i32
     let val = noop(0u32);
     show_size(val);
 }
